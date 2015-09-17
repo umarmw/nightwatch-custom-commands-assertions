bdff6c078c16f39ae011d615307398e72aba57e4
Umar Waliyullah <umarmw@users.noreply.github.com>
2015-09-17 16:42:31 +0400

    Create compareScreenshot.js
    Add compare screenshot ability using resemblejs

---
+++ js/assertions/compareScreenshot.js
@@ -0,0 +1,74 @@
+var resemblejs = require('node-resemble-js');
+var mkdirp = require('mkdirp');
+var fs = require('fs');
+
+exports.assertion = function(filename, path, msg) {
+    var path = path || 'test/screenshots/';
+
+    var baselinePath = path + '/baseline/' ,
+        resultPath = path + '/results/' ,
+        diffPath = path + '/diffs/' ;
+
+    var baselineImage = baselinePath + filename,
+        resultImage = resultPath + filename,
+        diffImage = diffPath + filename;
+
+    this.message = msg || 'Unexpected compareScreenshot error.';
+    this.expected =  0;   // misMatchPercentage tolerance default 0%
+
+    this.command = function(callback) {
+
+        if (!fs.existsSync(baselinePath)) {
+            mkdirp(baselinePath, function (err) {
+                if (err) console.error(err)
+            });
+        }
+
+        if (!fs.existsSync(diffPath)) {
+            mkdirp(diffPath, function (err) {
+                if (err) console.error(err)
+            });
+        }
+
+        // create new baseline photo if none exists
+        if (!fs.existsSync(baselineImage)) {
+            console.log('WARNING: Baseline Photo does NOT exist.');
+            console.log('Creating Baseline Photo from Result: ' + baselineImage);
+            fs.writeFileSync(baselineImage, fs.readFileSync(resultImage));
+        }
+
+        resemblejs
+            .outputSettings({
+                errorColor: {
+                  red: 225,
+                  green: 0,
+                  blue: 255
+                },
+                errorType: 'movement',
+                transparency: 0.1,
+                largeImageThreshold: 1200
+              });
+
+        resemblejs(baselineImage).compareTo(resultImage)
+            //.ignoreAntialiasing()
+            //.ignoreColors()
+            .onComplete(callback);
+
+        return this;
+    };
+
+    this.value = function(result) {
+        result.getDiffImage().pack().pipe(fs.createWriteStream(diffImage));
+        return parseFloat(result.misMatchPercentage, 10);  // value this.pass is called with
+    };
+
+    this.pass = function(value) {
+        var pass = value <= this.expected;
+        if (pass) {
+            this.message = 'Screenshots Matched for ' + filename +' with a tolerance of ' + this.expected + '%';
+        } else {
+            this.message = 'Screenshots Match Failed for ' + filename +' with a tolerance of ' + this.expected + '%';
+        }
+        return pass;
+    };
+};
